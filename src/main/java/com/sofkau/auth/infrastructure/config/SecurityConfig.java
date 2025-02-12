3476092ccbd81fdc7d86c106db18812693b66a68
Andrés Téllez <davidroa8@hotmail.com>
2025-02-12 00:21:47 -0500

    add initial security config and customer controller

---
+++ src/main/java/com/sofkau/auth/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sofkau.auth.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        return http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/swagger-ui/**", "**/api-docs/**").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                        .anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
---
+++ src/main/java/com/sofkau/auth/infrastructure/http/controllers/CustomerController.java
@@ -0,0 +1,43 @@
+package com.sofkau.auth.infrastructure.http.controllers;
+
+import com.sofkau.auth.application.dtos.CreateCustomerRequest;
+import com.sofkau.auth.application.dtos.GetCustomerResponse;
+import com.sofkau.auth.application.dtos.UpdateCustomerRequest;
+import com.sofkau.auth.application.ports.input.customer.CreateCustomer;
+import com.sofkau.auth.application.ports.input.customer.DeleteCustomer;
+import com.sofkau.auth.application.ports.input.customer.GetCustomer;
+import com.sofkau.auth.application.ports.input.customer.UpdateCustomer;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final GetCustomer getCustomer;
+    private final CreateCustomer createCustomer;
+    private final UpdateCustomer updateCustomer;
+    private final DeleteCustomer deleteCustomer;
+
+    @GetMapping("/{id}")
+    public GetCustomerResponse getCustomer(@PathVariable long id) {
+        return getCustomer.get(id);
+    }
+
+    @PostMapping
+    public GetCustomerResponse createCustomer(@RequestBody @Valid CreateCustomerRequest createCustomerRequest) {
+        return createCustomer.create(createCustomerRequest);
+    }
+
+    @PutMapping("/{id}")
+    public GetCustomerResponse updateCustomer(@PathVariable long id,
+                                              @RequestBody @Valid UpdateCustomerRequest updateCustomerRequest) {
+        return updateCustomer.update(id, updateCustomerRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable long id) {
+        deleteCustomer.delete(id);
+    }
+}
